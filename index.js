5878d063ba065c636625da107d1536d2cf21b689
DGMary <mblnkinakk@gmail.com>
2018-04-26 18:36:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 console.log("first commit");
+var deepArray = [
+	[1 , 2],
+	[3 , 4],
+	[5, 6]
+] 
+
+function arrayClone (arr) {
+	var i, copy;
+	
+	if (Array.isArray(arr)) {
+		copy = arr.slice(0);
+		for (i = 0; i < copy.length; i++) {
+			copy[i] = arrayClone(copy[i]);
+		}
+		return copy;
+	} else if(typeof arr === 'object') {
+		throw 'Cannot clone array containing an object!';
+	} else {
+		return arr;
+	}
+}
+
+console.log(arrayClone (deepArray));
